3f0670d7c548605bdad44d3a2a7d29afe4b70e8b
TinyColonel <69681605+NikitaVoitik@users.noreply.github.com>
2022-04-04 20:55:45 +0300

    Bug fixes

--- script.js
+++ script.js
@@ -1,52 +1,79 @@
 let num = 0;
 
+function Task(value, date) {
+    if (typeof (date) == 'object') {
+        let day = date.getDate();
+        let month = date.getMonth();
+        const year = date.getFullYear();
+        date = `${day}.${month}.${year}`;
+    }
+    this.value = value;
+    this.date = date;
+}
+
 const deleteAllCompletedTasks = () => {
-    if (!confirm("Are you sure you want to delete all COMPLETED tasks?"))
-        return;
     const tasks = document.getElementById("completedTasks");
-    tasks.innerHTML = "";
+    tasks.innerHTML = null;
 }
 
 const deleteAllTasks = () => {
-    if (!confirm("Are you sure you want to delete ALL tasks?"))
-        return;
     num = 0;
     const tasks = document.getElementById("main");
     tasks.innerHTML = `
-        <div class="uncompleted" id="uncompletedTasks">
-            
-        </div>
-        <div class="completed" id="completedTasks">
-            
-        </div>`;
+                       <div class="uncompleted" id="uncompletedTasks">
+                       </div>
+                       <div class="completed" id="completedTasks">  
+                       </div>
+                      `;
 }
 
-const getTemplate = (type, value, id) => {
+const getTemplate = (type, task, id) => {
     if (!type) {
+        //uncompleted task
         return `
                 <div class="block" id="${id}">
-                    <p class="taskText">${value}</p>
+                    <div>
+                        <p class="date" id="date">${task.date}</p>              
+                        <p class="taskText">${task.value}</p>
+                    </div>
                     <div class="taskButtons">
                         <a class="taskButton" href="#" onclick="makeCompleted(this.parentNode.parentNode.id)">Complete</a>
                         <a class="taskButton" href="#" onclick="deleteTask(this.parentNode.parentNode.id)">Delete</a>
                     </div>
-                </div>`;
+                </div>
+               `;
     } else {
-        return `<div class="block" id="${id}">
-                    <p class="taskText">${value}</p>
+        //completed task
+        return `
+                <div class="block" id="${id}">
+                    <div>
+                        <p class="date" id="date">${task.date}</p>              
+                        <p class="taskText">${task.value}</p>
+                    </div>
                     <div class="taskButtons">
                         <a class="taskButton" href="#" onclick="makeUncompleted(this.parentNode.parentNode.id)">Uncomplete</a>
                         <a class="taskButton" href="#" onclick="deleteTask(this.parentNode.parentNode.id)">Delete</a>
                     </div>
-                </div>`;
+                </div>
+                `;
     }
 }
 
 const addNewTask = () => {
-    num++;
     const input = document.forms.formMakeTask.makeTask;
-    const task = input.value;
-    input.value = "";
+    const task = new Task(input.value, new Date());
+    input.value = null;
+    let empty = true;
+    for (let char of task.value) {
+        if (char !== ' ') {
+            empty = false;
+            break;
+        }
+    }
+    if (empty){
+        return false;
+    }
+    num++;
     const beginOfUncompleted = document.getElementById("uncompletedTasks");
     beginOfUncompleted.insertAdjacentHTML('afterbegin', getTemplate(0, task, num));
     return false;
@@ -54,18 +81,22 @@ const addNewTask = () => {
 
 const deleteTask = (id) => {
     const task = document.getElementById(id);
-    task.outerHTML = "";
+    task.outerHTML = null;
 }
 
 const makeCompleted = (id) => {
-    const task = document.getElementById(id).firstElementChild.innerHTML;
+    const information = document.getElementById(id).firstElementChild.children;
+    const task = new Task(information[1].innerHTML, information[0].innerHTML);
+    console.log(task);
     deleteTask(id);
     const beginOfCompleted = document.getElementById("completedTasks");
     beginOfCompleted.insertAdjacentHTML('afterbegin', getTemplate(1, task, id));
 }
 
 const makeUncompleted = (id) => {
-    const task = document.getElementById(id).firstElementChild.innerHTML;
+    const information = document.getElementById(id).firstElementChild.children;
+    const task = new Task(information[1].innerHTML, information[0].innerHTML);
+    console.log(task);
     deleteTask(id);
     const beginOfUncompleted = document.getElementById("uncompletedTasks");
     beginOfUncompleted.insertAdjacentHTML('afterbegin', getTemplate(0, task, id));
